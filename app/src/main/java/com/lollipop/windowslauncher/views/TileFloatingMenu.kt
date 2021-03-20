29a286815b4298096f4c2bddce7b5cb6b19e29a8
lollipop <liujian594@hotmail.com>
2021-03-20 19:45:52 +0800

    增加磁块的悬浮菜单

--- app/src/main/java/com/lollipop/windowslauncher/views/TileFloatingMenu.kt
+++ app/src/main/java/com/lollipop/windowslauncher/views/TileFloatingMenu.kt
@@ -1,10 +1,18 @@
 package com.lollipop.windowslauncher.views
 
+import android.view.View
+
 /**
  * @author lollipop
  * @date 3/19/21 22:43
  * 瓷砖的悬浮菜单
  */
 class TileFloatingMenu {
+
+
+
+    fun showIn(anchor: View) {
+
+    }
 
 }
