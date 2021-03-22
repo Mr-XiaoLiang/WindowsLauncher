4bd40c45fa61e9e4de1417882c4c6c5371cc0e95
lollipop <liujian594@hotmail.com>
2021-03-22 22:45:40 +0800

    增加磁块的悬浮菜单

--- app/src/main/java/com/lollipop/windowslauncher/views/TileFloatingMenu.kt
+++ app/src/main/java/com/lollipop/windowslauncher/views/TileFloatingMenu.kt
@@ -1,9 +1,12 @@
 package com.lollipop.windowslauncher.views
 
-import android.content.Context
 import android.view.View
+import android.view.ViewGroup
 import android.widget.FrameLayout
+import androidx.core.view.ViewCompat
+import androidx.recyclerview.widget.RecyclerView
 import com.lollipop.windowslauncher.tile.TileSize
+import kotlin.math.min
 
 /**
  * @author lollipop
@@ -13,17 +16,39 @@ import com.lollipop.windowslauncher.tile.TileSize
 class TileFloatingMenu private constructor(private val option: Option) {
 
     companion object {
+        /**
+         * 纵向列表的菜单
+         */
         fun listMenu(): Builder {
             return Builder(Style.List)
         }
 
+        /**
+         * 横向方块的菜单
+         */
         fun blockMenu(): Builder {
             return Builder(Style.Block)
         }
     }
 
-    private val rootGroup: View by lazy {
+    /**
+     * 根结点
+     */
+    private val rootGroup: ViewGroup by lazy {
         AnchorLayoutGroup(option.anchor)
+    }
+
+    /**
+     * 列表的View
+     */
+    private val listView: RecyclerView by lazy {
+        RecyclerView(option.anchor.context).apply {
+            rootGroup.addView(
+                this,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+        }
     }
 
     private fun show() {
@@ -33,13 +58,56 @@ class TileFloatingMenu private constructor(private val option: Option) {
 
     private class AnchorLayoutGroup(
         val anchor: View
-    ): FrameLayout(anchor.context) {
+    ) : FrameLayout(anchor.context) {
 
+        override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+            super.onLayout(changed, left, top, right, bottom)
+            getLayoutOffset { isTop, maxHeight, baseLine ->
+                for (index in 0 until childCount) {
+                    getChildAt(index)?.let { child ->
+                        val childHeight = min(maxHeight, child.measuredHeight)
+                        if (childHeight != child.measuredHeight) {
+                            // 如果尺寸不一致，那么再测量一次
+                            child.measure(
+                                MeasureSpec.makeMeasureSpec(child.width, MeasureSpec.EXACTLY),
+                                MeasureSpec.makeMeasureSpec(childHeight, MeasureSpec.EXACTLY),
+                            )
+                        }
+                        val offsetY = if (isTop) {
+                            baseLine - childHeight - child.top
+                        } else {
+                            baseLine - child.top
+                        }
+                        ViewCompat.offsetTopAndBottom(child, offsetY)
+                    }
+                }
+            }
+        }
 
+        private fun getLayoutOffset(
+            callback: (isTop: Boolean, maxHeight: Int, baseLine: Int) -> Unit
+        ) {
+            val anchorLocation = anchor.getLocationInWindow()
+            val myLocation = this.getLocationInWindow()
+
+            val topSpace = anchorLocation[1] - myLocation[1]
+            val bottomSpace = height - topSpace - anchor.height
+            if (topSpace > bottomSpace) {
+                callback(true, topSpace, topSpace)
+            } else {
+                callback(false, bottomSpace, height - bottomSpace)
+            }
+        }
+
+        private fun View.getLocationInWindow(): IntArray {
+            val location = IntArray(2)
+            getLocationInWindow(location)
+            return location
+        }
 
     }
 
-    class Builder (val style: Style) {
+    class Builder(val style: Style) {
         private val resizeList = ArrayList<TileSize>()
         private val buttonList = ArrayList<ButtonInfo>()
         private var clickListener: ((id: Int) -> Unit)? = null
