be0f1209402287393a742e6cdf84d9d2724f67dc
lollipop <liujian594@hotmail.com>
2021-04-08 23:23:57 +0800

    增加拖拽的Group

---
+++ app/src/main/java/com/lollipop/windowslauncher/views/DragGroup.kt
@@ -0,0 +1,19 @@
+package com.lollipop.windowslauncher.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+/**
+ * @author lollipop
+ * @date 4/8/21 23:21
+ * 拖拽的容器，它会在需要的时候拦截事件，
+ * 并且拖拽传递事件
+ */
+class DragGroup(context: Context, attrs: AttributeSet?, style: Int) :
+    FrameLayout(context, attrs, style) {
+
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context) : this(context, null)
+
+}
